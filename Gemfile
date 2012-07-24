878944bfd3210570b633287dac5ebbaac1d43cdd
Jeff Montgomery <poolstud1970@hotmail.com>
2012-07-23 20:05:04 -0400

    Initial code checkin

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'pg','0.12.2'
+
+group :development, :test do
+  gem 'rspec-rails', '2.10.0'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.4'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+gem 'therubyracer'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a5bf82b6fa3731bb411ed3741d005941895bb5cda4a96e18678e72e9507d23ab5438f962bc78c6a65c63ac17772cf5e501ff0328a630b2dac00e71cf2fa8513c'
